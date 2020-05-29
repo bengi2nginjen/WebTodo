14d873e6b537890e90d7b19c906d2ab429c2116e
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-05-29 19:02:11 +0700

    uji coba

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
       <!-- MESSAGES -->
 
       <?php if (isset($_SESSION['message'])) { ?>
+      <h1>Coba baru push</h1>
       <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
         <?= $_SESSION['message']?>
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
