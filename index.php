64d827cae1f0b551a32d8706388d05dee228bba1
bengi2nginjen <eusebia.fernanda@ti.ukdw.ac.id>
2020-05-30 16:20:39 +0700

    tambah bruh

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             <input type="text" name="title" class="form-control" placeholder="Task Title Bruh" autofocus>
           </div>
           <div class="form-group">
-            <textarea name="description" rows="2" class="form-control" placeholder="Task Description"></textarea>
+            <textarea name="description" rows="2" class="form-control" placeholder="Task Description Bruh"></textarea>
           </div>
           <input type="submit" name="save_task" class="btn btn-success btn-block" value="Save Task">
         </form>
